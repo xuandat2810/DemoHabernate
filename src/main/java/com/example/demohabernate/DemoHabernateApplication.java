8d983b2060a938cd4d0c4281f6bd03e6f6c2db1c
xuandat28105 <xuandat28105@gmail.com>
2021-08-24 14:17:33 +0700

    Initial commit

---
+++ src/main/java/com/example/demohabernate/DemoHabernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.demohabernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHabernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHabernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demohabernate/DemoHabernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demohabernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHabernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
